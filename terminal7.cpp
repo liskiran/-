d5a27096811dba8dd1f439f24766608de74cec22
liskiran <52342496+liskiran@users.noreply.github.com>
2020-12-13 20:09:34 +0700

    Update terminal7.cpp

--- terminal7.cpp
+++ terminal7.cpp
@@ -1,133 +1,156 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void print(float matrix[40][40], int row, int col) {
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++)
-            printf("  %0.2f", matrix[i][j]);
-        printf("\n");
-    }
+void print(double a[40][40], int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++)
+			printf("  %0.2f", a[i][j]);
+		printf("\n");
+	}
 }
 
-void swap(float matrix[40][40], int row1, int row2, int col) {
-    for (int i = 0; i < col; i++) {
-        float temp = matrix[row1][i];
-        matrix[row1][i] = matrix[row2][i];
-        matrix[row2][i] = temp;
-    }
-}
-bool is_zero_rows(float matrix[40][40], int rows, int cols) {
-    for(int i = 0; i < cols; i++) {
-        if(matrix[rows][i] != 0) {
-            return false;
-        }
-    }
-    return true;
-}
+void gauss_method(double a[40][40], int m, int n) {
+	int flag = 0;
+	double x = 0, r = 0;
+	//перестановка строк чтобы на главной диагонали нулей не было
+	for (int i = 0; i < m; i++) {
+		for (int j = i; j < m; j++) {
+			if ((a[j][i] != 0.0) && (j != i)) {
+				for (int m = 0; m < n; m++) {
+					// меняем строки
+					r = a[j][m]; 
+					a[j][m] = a[i][m]; 
+					a[i][m] = r;
+				}
+				break;
+			}
+			if ((a[j][i] != 0.0) && (j == i)) break;
+		}
+	}
+	//приводим к ступенчатому виду
+	int m1;
+	if (m > n)
+		m1 = n;
+	else 
+		m1 = m;
+	// гаусс вниз и вверх для прямоугольных и квадратных матриц
+		for (int i = 0; i < m1; i++) {
+			if (a[i][i] != 0.0) {
+				for (int j = i + 1; j < m1; j++) {
+					x = a[j][i] / a[i][i];
+					for (int l = 0; l < n; l++) {
+						a[j][l] = a[j][l] - x * a[i][l];
+					}
+				}
+			}
+		}
+		for (int i = m1 - 1; i > 0; i--) {
+			if (a[i][i] != 0.0) {
+				for (int j = i - 1; j >= 0; j--) {
+					x = a[j][i] / a[i][i];
+					for (int l = 0; l < n; l++) {
+						a[j][l] = a[j][l] - x * a[i][l];
+					}
+				}
+			}
+		}
 
-void gauss_method(float matrix[40][40], int rows, int cols) {
-    int rank = cols;
-    for (int row = 0; row < rank; row++) {
-        if(is_zero_rows(matrix, row, cols)) {
-            break;
-        }
-        if (row < cols && row < rows && matrix[row][row] != 0) {
-            for (int col = 0; col < rows; col++) {
-                if (col != row) {
-                    float mult = matrix[col][row] / matrix[row][row];
-                    
-                    for (int i = 0; i < rank; i++)
-                        matrix[col][i] -= mult * matrix[row][i];
-                }
-            }
-        } else {
-            bool reduce = true;
-            for (int i = row + 1; i < rows; i++){
-                if (matrix[i][row] != 0){
-                    swap(matrix, row, i, rank);
-                    reduce = false;
-                    break;
-                }
-            }
-            if (reduce) {
-                rank--;
-                for (int i = 0; i < rows; i++)
-                    matrix[i][row] = matrix[i][rank];
-            }
-            row--;
-        }
-        //printf("\n");
-       // print(matrix, rows, cols);
-        //printf("\n");
-    }
+	int kol = 0;
+	// строки равные нулю отправляем вниз матрицы
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (a[i][j] != 0.0) {
+				kol += 1;
+			}
+		}
+		// если строка равна нулевая перемещаем вниз
+		if (kol == 0) {
+			for (int h = i; h < m - 1; h++) {
+				for (int l = 0; l < n; l++) {
+					a[h][l] = a[h + 1][l];
+					a[h + 1][l] = 0;
+				}
+			}
+		}
+		kol = 0;
+	}
 }
-void find_fundamental_solution(float matrix[40][40], int rows, int cols) {
-    int count_params = cols - rows;
-    int peremennie = count_params;
-    float params[40];
-    for(int i = 0; i < 40; i++) {
-        params[i] = 0;
-    }
-    int c = 0;
-    float res = 0;
-    float value;
-    for(int i = 0; i < rows && peremennie != 0; i++) {
-        res = 0;
-         for(int j = 0; j < cols; j++) {
-             if(matrix[i][j] != 0) {
-                value = matrix[i][j];
-                int count_no_zero = 0;
-                j += 1;
-                while(j < cols) {
-                    if(matrix[i][j] != 0) {
-                        res -= matrix[i][j] * 1; 
-                        count_no_zero++;
-                    }
-                    j++;
-                }
-                params[c] = res / value;
-                c += 1;
-                peremennie--;
-                if(count_no_zero == 1) {
-                    params[c - 1] = 0;
-                }
-                break;
-             }
-         }
-    }
-    for(int i = 0; i < cols; i++) {
-        printf("%0.2f  ", params[i]);
-    }
-}
-int count_zero_rows(float matrix[40][40], int rows, int cols) {
-    int result = rows;
-    int c = 0;
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
-            if(matrix[i][j] != 0) c++;
-        }
-        if(c == 0) result--;
-        c = 0;
-    }
-    return result;
+void find_fundamental_solution(double a[40][40], int n, int m) {
+	gauss_method(a, m, n);
+	int b[40] = { 0 };
+	double ix[40] = { 0 };
+	int niz = 0;
+	int kol = 0;
+	//ищем нижнюю ненулевую строку
+	for (int i = m - 1; i >= 0; i--) {
+		for (int j = 0; j < n; j++) {
+			if (a[i][j] != 0.0) {
+				niz = i;
+				break;
+			}
+		}
+		if (niz != 0) break;
+	}
+	//printf("%d\n",niz);
+	int sum = 0;
+	//ищем зависимые переменные
+	for (int i = niz; i >= 0; i--) {
+		for (int j = 0; j < n; j++) {
+			if (a[i][j] != 0.0) {
+				b[j] += 1;
+				sum += 1;
+				break;
+			}
+		}
+	}
+	int in = 1;
+	printf("Fundamental system of solutions\n\n");
+	// если сумма равно n, то тогда нулевой столбец единственное решение
+	if (sum == n) {
+		printf("1) ");
+		for (int i = 0; i < n; i++) {
+			printf("%f ", 0);
+		}
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (b[i] == 0) {
+			ix[i] = 1;
+			for (int j = niz; j >= 0; j--) {
+				for (int l = 0; l < n; l++) {
+					if (a[j][l] != 0.0) {
+						// тут находим базисные корни 
+						for (int h = l + 1; h < n; h++) {
+							ix[l] += ix[h] * a[j][h] * (-1);
+						}
+						ix[l] = ix[l] / a[j][l];
+					}
+					if (a[j][l] != 0.0) break;
+				}
+			}
+			printf("%d) ", in);
+			in += 1;
+			for (int m = 0; m < n; m++) {
+				printf("%lf ", ix[m]);
+			}
+			printf("\n");
+			for (int q = 0; q < n; q++) ix[q] = 0;
+		}
+	}
 }
 int main() {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    int n, k;
-    scanf("%d%d", &n, &k); // Input dimension of the matrix
-    float matrix[40][40];
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int n, m;
+	scanf("%d%d", &n, &m); //  Введите размерность матрицы
+	double a[40][40];
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < k; j++) {
-            scanf("%f", &matrix[i][j]); // Input values of matrix
-        }
-    }
-    printf("Before:\n");
-    print(matrix, n, k);
-    gauss_method(matrix, n, k);
-    printf("\nAfter:\n");
-    print(matrix, n, k);
-    printf("\n");
-    find_fundamental_solution(matrix, count_zero_rows(matrix, n, k), k);
-    return 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%lf", &a[i][j]); // Введите значения матрицы
+		}
+	}
+	find_fundamental_solution(a, m, n);
+	
+	return 0;
 }
